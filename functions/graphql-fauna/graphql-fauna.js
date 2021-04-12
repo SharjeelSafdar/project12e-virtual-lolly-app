b31e648cb3f7a543d56f0596b62e8a60dee8de3c
Mian Muhammad Sharjeel Safdar <miansharjeelsafdar@gmail.com>
2021-04-13 02:00:24 +0500

    Enabbles GraphiQL playground in production

--- functions/graphql-fauna/graphql-fauna.js
+++ functions/graphql-fauna/graphql-fauna.js
@@ -85,8 +85,8 @@ const resolvers = {
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  // playground: true,
-  // introspection: true,
+  playground: true,
+  introspection: true,
 });
 
 exports.handler = server.createHandler();
