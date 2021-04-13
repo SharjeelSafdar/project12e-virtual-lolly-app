dd6df61358cd42071f841815d3681a7ae93fa915
Mian Muhammad Sharjeel Safdar <miansharjeelsafdar@gmail.com>
2021-04-14 03:47:58 +0500

    Re-added createPages API

--- src/gatsby/node.ts
+++ src/gatsby/node.ts
@@ -1,41 +1,41 @@
-// import { GatsbyNode } from "gatsby";
+import { GatsbyNode } from "gatsby";
 import { Lolly } from "../types";
 
-// export const createPages: GatsbyNode["createPages"] = async ({
-//   graphql,
-//   actions,
-// }) => {
-//   const { data }: QueryResponse = await graphql(`
-//     query GetAllLollies {
-//       lolly {
-//         getAllLollies {
-//           id
-//           topColor
-//           middleColor
-//           bottomColor
-//           recipientName
-//           message
-//           sendersName
-//         }
-//       }
-//     }
-//   `);
+export const createPages: GatsbyNode["createPages"] = async ({
+  graphql,
+  actions,
+}) => {
+  const { data }: QueryResponse = await graphql(`
+    query GetAllLollies {
+      lolly {
+        getAllLollies {
+          id
+          topColor
+          middleColor
+          bottomColor
+          recipientName
+          message
+          sendersName
+        }
+      }
+    }
+  `);
 
-//   if (data && data.lolly) {
-//     const lollies = data.lolly.getAllLollies;
-//     lollies.forEach(lolly => {
-//       actions.createPage({
-//         path: `/lolly/${lolly.id}`,
-//         component: require.resolve("../templates/showLolly"),
-//         context: {
-//           ...lolly,
-//         },
-//       });
-//     });
-//   }
-// };
+  if (data && data.lolly) {
+    const lollies = data.lolly.getAllLollies;
+    lollies.forEach(lolly => {
+      actions.createPage({
+        path: `/lolly/${lolly.id}`,
+        component: require.resolve("../templates/showLolly"),
+        context: {
+          ...lolly,
+        },
+      });
+    });
+  }
+};
 
-export type QueryResponse = {
+type QueryResponse = {
   data?: {
     lolly: {
       getAllLollies: Lolly[];
